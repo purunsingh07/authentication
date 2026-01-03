3acdbd09fb0608a3c4801080f71d7eba7e747677
purunsingh07 <purunsingh07@gmail.com>
2026-01-03 17:25:35 +0530

    completed login and signup with frontend and backend working on verify email

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React,{ useContext } from 'react'
 import { assets } from '../assets/assets'
+import { AppContext } from '../context/AppContext'
+import axios from 'axios'
 
 const Header = () => {
+
+  const {userData} = useContext(AppContext)
   return (
     <div className='flex flex-col items-center mt-20 px-4 text-center text-gray-800'>
       <img src={assets.header_img} alt=""  className='w-36 h-36 rounded-full mb-6'/>
-      <h1 className='flex items-center gap-2 text-xl sm:text-3xl font-medium mb-2'>Hey Developer
+      <h1 className='flex items-center gap-2 text-xl sm:text-3xl font-medium mb-2'>Hey {userData ? userData.name : 'Developer'}!
       <img className='w-8 aspect-square' src={assets.hand_wave} alt="" />
       </h1>
 
